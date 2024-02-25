92e30610174260a7559f3fa28fb35cf8272c9bea
Rodney <rondineypatricio@gmail.com>
2024-02-24 21:14:53 -0300

    More changes

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,17 +1,16 @@
 const lists = document.querySelectorAll(".list");
 const subItems = {
-  Race: ['Elf', 'Dwarf', 'Human'],
-  Classes: ['Warrior', 'Mage', 'Cleric'],
-  Antecedent: ['Acolyte', 'Anthropologist', 'Athlete'],
-  Equipament: ['Sword', 'Shield', 'Armor'],
-  Magias: ['Fireball', 'Sacred Flame', 'Concert']
+  Race: ['Elf','Dwarf','Human'],
+  Classes: ['Warrior','Mage', 'Cleric'],
+  Antecedent: ['Acolyte', 'Anthropologist','Athlete'],
+  Equipament: ['Sword','Shield', 'Armor'],
+  Magias: ['Fireball','Sacred Flame', 'Concert']
 }
 
 lists.forEach((list) => {
   list.addEventListener('click', () => {
-    list.classList.toggle(".listOnClick");
     let listName = list.textContent.trim(); 
-
+    list.classList.toggle("active");
     // Se a lista já foi preenchida, é limpado
     if (list.children.length > 1) {
       while (list.children.length > 1) {
@@ -23,7 +22,7 @@ lists.forEach((list) => {
     if (subItems[listName]) {
       for (let i = 0; i < 3; i++) {
         let newSubItem = document.createElement('li');
-        newSubItem.textContent = subItems[listName][i];
+        newSubItem.innerHTML = subItems[listName][i] + "<br>";
         list.appendChild(newSubItem);
       }
     }
